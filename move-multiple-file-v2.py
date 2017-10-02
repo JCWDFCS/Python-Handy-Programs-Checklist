00140c6e2297c02b526308358e21b4faf43ed38c
JCWDFCS <342143690@qq.com>
2017-10-02 15:27:38 +0800

    add move-multiple-files-v2

---
+++ move-multiple-file-v2.py
@@ -0,0 +1,34 @@
+import os
+import glob
+
+
+
+
+# change to the current path,veneer techniques
+def change_to_current_directory(src_path):
+    os.chdir(src_path)
+
+def filter_files(wildcard_ext,src_path):
+    files = glob.glob(src_path + wildcard_ext)
+    # Avoid repeated traverse the directory.
+    return files[:]
+
+
+def move_files(wildcard_ext,src_path,dst_path):
+     # filter the qualified files
+    change_to_current_directory(src_path)
+    src_files = filter_files(wildcard_ext,src_path)
+    cmd = 'mv %s %s'
+    for file in src_files:
+        print(dst_path + wildcard_ext)
+        os.popen(cmd %(file, dst_path))
+
+def main():
+    download_path =    '/Users/gaowei/Downloads/'
+    diary_images_path ='/Users/gaowei/Documents/Diary/images/'
+    image_ext =        '*.JPG'
+    # move .ext file from src_dir to dst_dir
+    move_files(image_ext,download_path,diary_images_path)
+
+if __name__ == '__main__':
+    main()
